391e977e0dd3e332668b3d1a8890d9904c1236d2
Angel <angelSistemas21@gmail.com>
2022-05-06 00:08:16 -0500

    feat: Se agrega un endpoint que regrese una lista de explorers

--- app.js
+++ app.js
@@ -2,9 +2,21 @@
 const express = require('express')
 // App de express
 const app = express()
-app.use(express.json) //Indicamos que usaremos JSON
+app.use(express.json()) //Indicamos que usaremos JSON
 // Puerto en que vamos a ver nuestra app "localhost:3000"
 const port = 3000
+
+// HTTP METHODS
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers GET ALL request ${new Date()}`)
+    const explorer1 = {id: 1, name: "Angel 1"}
+    const explorer2 = {id: 1, name: "Angel 2"}
+    const explorer3 = {id: 1, name: "Angel 3"}
+    const explorer4 = {id: 1, name: "Angel 4"}
+
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    res.status(200).json(explorers)
+})
 
 // Con esto inicializamos esta app
 app.listen(port, () => {
